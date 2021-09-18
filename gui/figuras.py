ffb9dc54a33c5ef491358d3d49248514a1253d2d
Ivan Vera <ivera2065@fpuna.edu.py>
2021-09-18 01:24:21 -0400

    actualizacion del algoritmo minimax

--- gui/figuras.py
+++ gui/figuras.py
@@ -1,27 +1,132 @@
 import pygame
 
-class Button:
+movimientos = [(0,-1), (0,1), (-1,-1), (1,1), (-1,0), (1,0), (1,-1), (-1,1)] 
+
+
+# 1 Blanca, -1 Negro, None vacio
+class Ficha:
+    def __init__(self, row, col, color):
+        self.x = 70 * col + 35
+        self.y = 70 * row + 35 
+        self.color = color 
+
+        if self.color == 1:
+            self.cod_color = "#FFFFFF"
+        else:
+            self.cod_color = "#000000"
+
+    def pintarFicha(self, pantalla):
+        pygame.draw.circle(pantalla, self.cod_color, (self.x,self.y), 30) 
     
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y 
-        self.width = width
-        self.height = height 
-        self.btn = pygame.Rect((self.x,self.y), (self.width, self.height))
 
-    def draw(self, screen, color, b_radius):
-        pygame.draw.rect(screen, color, self.btn, border_radius=b_radius) 
+class Tablero: 
+    def __init__(self):
+        self.matriz = [None]*8
+        for i in range(8):
+            self.matriz[i] = [None]*8
 
-
-class Text:
-
-    def __init__(self, text, size, color):
-        self.font = pygame.font.SysFont("notomono", size, True)
-        self.text = self.font.render(text, True, color) 
+        self.matriz[3][3] = Ficha(3,3,1)
+        self.matriz[3][4] = Ficha(3,4,-1)
+        self.matriz[4][3] = Ficha(4,3,-1)
+        self.matriz[4][4] = Ficha(4,4,1)
     
-    def get(self):
-        return self.text
-        
+    def pintarTablero(self, pantalla):
+        for row in range(0,630,70):
+            for col in range(0,630,70):
+                pygame.draw.rect(pantalla, "#000000", [row,col,70,70], 1) 
+
+    def colocarFichas(self, pantalla):
+        for f in range(8):
+            for c in range(8):
+                if self.matriz[f][c] != None:
+                    ficha = self.matriz[f][c] 
+                    ficha.pintarFicha(pantalla) 
+    
+    def validarMovimientos(self, row, col, pieza):
+        if self.matriz[row][col] != None:
+            return False
+        else:
+            oponente = -pieza 
+            for pos in movimientos:
+                if self.validarDireccion(row+pos[0] ,col+pos[1]) and self.checkearVolteo(row+pos[0] ,col+pos[1], pos, pieza, oponente):
+                    return True
+
+    def checkearVolteo(self, x, y, pos, pieza, oponente):
+        if self.matriz[x][y] and self.matriz[x][y].color == oponente:
+            while self.validarDireccion(x,y):
+                x += pos[0]
+                y += pos[1]
+                if self.matriz[x][y] == None:
+                    return False 
+                elif self.matriz[x][y].color == pieza:
+                    return True 
+        else:
+            return False    
+    
+    def validarDireccion(self, x, y):
+        if x >= 0 and x < 8 and y >= 0 and y < 8:
+            return True
+        else:
+            return False 
+
+    def voltearFichas(self, row, col, pieza):
+        oponente = -pieza 
+        for pos in movimientos:
+            if self.checkearVolteo(row+pos[0] ,col+pos[1], pos, pieza, oponente):
+                self.cambioColor(row+pos[0] ,col+pos[1], pos, pieza, oponente)
+    
+    def cambioColor(self, x, y, pos, pieza, oponente):
+        while self.validarDireccion(x,y) and self.matriz[x][y].color == oponente:
+            self.matriz[x][y] = Ficha(x,y,pieza)
+            x += pos[0]
+            y += pos[1] 
+    
+    def contarFichas(self, pieza):
+        posiciones = []
+        for f in range(8):
+            for c in range(8):
+                if self.matriz[f][c] and self.matriz[f][c].color  == pieza:
+                    posiciones.append((f,c))
+        return posiciones 
+
+    def getMovimientos(self, x, y, destino, oponente):
+        if self.matriz[x][y] and self.matriz[x][y].color  == oponente:
+            while x >= 0 and x < 8 and y >= 0 and y < 8:
+                x += destino[0]
+                y += destino[1]
+                if self.matriz[x][y] == None:
+                    return (x,y) 
+                elif self.matriz[x][y].color == -oponente:
+                    return False
+        else:
+            return False 
+
+
+    def movimientosPosibles(self, pieza):
+        movimientos_legales = []
+        for origin in self.contarFichas(pieza):
+            for destino in movimientos:
+                x, y = origin[0] + destino[0], origin[1] + destino[1]
+                pos = self.getMovimientos(x, y, destino, -pieza)
+                if pos:
+                    movimientos_legales.append(pos)
+        return movimientos_legales 
+
+    def listaMovimientos(self, pieza):
+        numMovimientos = 0
+        for f in range(8):
+            for c in range(8):
+                if self.validarMovimientos(f, c, pieza):
+                    numMovimientos += 1 
+        return numMovimientos
+
+    def finDeJuego(self):
+        numBlanco = self.listaMovimientos(1)
+        numNegro = self.listaMovimientos(-1)
+        if numBlanco == 0 and numNegro == 0:
+            return True 
+        else:
+            return False
 
     
 
--- gui/view.py
+++ gui/view.py
@@ -1,5 +1,78 @@
-import pygame, sys
+import pygame, sys, os 
+from copy import deepcopy
+from figuras import Tablero, Ficha
 
+tablero_pesos = [
+    [100, -10, 11, 6, 6, 11, -10, 100],
+    [-10, -20, 1, 2, 2, 1, -20, -10],
+    [10, 1, 5, 4, 4, 5, 1, 10],
+    [6, 2, 4, 2, 2, 4, 2, 6],
+    [6, 2, 4, 2, 2, 4, 2, 6],
+    [10, 1, 5, 4, 4, 5, 1, 10],
+    [-10, -20, 1, 2, 2, 1, -20, -10],
+    [100, -10, 11, 6, 6, 11, -10, 100]
+]
+
+
+def funcion_evaluacion(tablero, color):
+    oponente = -color 
+    total = 0 
+
+    coordenadas_jugador = tablero.contarFichas(color) 
+    for pos in coordenadas_jugador:
+        total += tablero_pesos[pos[0]][pos[1]] 
+    
+    coordenadas_op = tablero.contarFichas(oponente) 
+    for pos in coordenadas_op:
+        total -= tablero_pesos[pos[0]][pos[1]]
+
+    return total
+
+
+def min_valor(estado):
+
+    if estado.finDeJuego():
+        return (funcion_evaluacion(estado,1), None)
+    
+    mejor_utilidad = sys.maxsize 
+    mejor_movimiento = None 
+    
+    for move in estado.movimientosPosibles(1):
+        nuevo_estado = deepcopy(estado)
+        nuevo_estado.voltearFichas(move[0],move[1],1)
+        tupla = max_valor(nuevo_estado)
+        if tupla[0] < mejor_utilidad:
+            mejor_utilidad = tupla[0]
+            mejor_movimiento = move
+    return (mejor_utilidad, mejor_movimiento)
+
+
+def max_valor(estado):
+    if estado.finDeJuego():
+        return (funcion_evaluacion(estado,-1),None)
+
+    mejor_utilidad = -sys.maxsize
+    mejor_movimiento = None 
+    
+    for move in estado.movimientosPosibles(-1):
+        nuevo_estado = deepcopy(estado)
+        nuevo_estado.voltearFichas(move[0],move[1],-1)
+        tupla = min_valor(nuevo_estado)
+        if tupla[0] > mejor_utilidad:
+            mejor_utilidad = tupla[0]
+            mejor_movimiento = move
+    return (mejor_utilidad, mejor_movimiento)
+
+
+def minimax(estado):
+    mejor_movimiento = max_valor(estado)[1]
+    return mejor_movimiento
+
+
+tablero = Tablero()
+print(minimax(tablero))
+
+"""
 pygame.init()
 
 WIDTH = HEIGHT = 560
@@ -18,10 +91,23 @@ menu_main = True
 juego = False
 
 while True:
+
+    time.tick(90)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            sys.exit() 
+
+        if event.type == pygame.MOUSEBUTTONDOWN and not menu_main:
+            x, y = pygame.mouse.get_pos()
+            row = y // 70
+            col = x // 70 
+            if tablero.validarMovimientosJugador(row,col,1):
+                tablero.matriz[row][col] = Ficha(row,col,1)
+                tablero.voltearFichas(row,col,1)
+            else:
+                print("Invalido")
  
     pos = pygame.mouse.get_pos() 
 
@@ -40,15 +126,10 @@ while True:
         pantalla.blit(text_font, (30 + int((160 -text_font.get_width()) / 2),50+(int((50-text_font.get_height())) / 2)))
     elif juego:
         pantalla.fill("#008000")
-        pygame.draw.circle(pantalla, "#FFFFFF", (175,35), 30)
-        x_pos = 0
-        y_pos = 0
-        while y_pos < 560:          
-            pygame.draw.rect(pantalla, "#000000", [x_pos,y_pos,70,70], 1)
-            x_pos += 70
-            if x_pos == 560:
-                x_pos = 0
-                y_pos += 70
-    time.tick(40)
+        tablero.pintarTablero(pantalla)
+        tablero.colocarFichas(pantalla)
+        
+    
     pygame.display.update()
 
+"""
